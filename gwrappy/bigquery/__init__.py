4d1bf62ba006ad512fb6f58f24d7efd62defff73
Daniel Poon <daniel@wego.com>
2016-08-12 14:06:00 +0800

    import * from utils

--- gwrappy/bigquery/__init__.py
+++ gwrappy/bigquery/__init__.py
@@ -1,1 +1,2 @@
 from gwrappy.bigquery.bigquery import BigqueryUtility
+from gwrappy.bigquery.utils import *
--- gwrappy/storage/__init__.py
+++ gwrappy/storage/__init__.py
@@ -1,1 +1,2 @@
 from gwrappy.storage.storage import GcsUtility
+from gwrappy.storage.utils import *
