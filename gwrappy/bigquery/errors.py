7c3c597c135ad4da51418e2b9f4352a3e5bb99e7
Daniel Poon <daniel@wego.com>
2016-12-14 18:51:32 +0800

    - Added job id to error messages - Removed unnecessary reduce function for Python 3 compatibility

--- gwrappy/bigquery/errors.py
+++ gwrappy/bigquery/errors.py
@@ -5,16 +5,20 @@ class JobError(Error):
     """Job status done but error present."""
 
     def __init__(self, resp):
-        error_result = reduce(dict.__getitem__, ['status', 'errorResult'], resp)
+        error_result = resp['status']['errorResult']
 
+        self.id = resp['id']
         self.message = error_result.get('message', None)
         self.reason = error_result.get('reason', None)
         self.location = error_result.get('location', None)
         self.errors = resp['status'].get('errors', [])
 
     def __repr__(self):
-        return '<JobError %s in %s: "%s">' % (
-            self.reason, self.location, self.message
+        return '<JobError (%s) %s in %s: "%s">' % (
+            self.id,
+            self.reason,
+            self.location,
+            self.message
         )
 
     __str__ = __repr__
